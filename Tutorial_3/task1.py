09f446051faf580d0ab3cd27ca850971f735778f
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-05-06 17:54:49 +0200

    update

--- Tutorial_3/task1.py
+++ Tutorial_3/task1.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-
+from scipy import interpolate
 #open data
 data = open("data.txt", "r")
 
@@ -11,57 +11,55 @@ get_data = [line.strip().split() for line in data]
 
 print get_data
 
-xdata=[]
-ydata=[]
+xdata=np.array([])
+ydata=np.array([])
 
 for i in range(len(get_data)):
 	x = get_data[i][0]	
 	y = get_data[i][1]
-	xdata.append(x)
-	ydata.append(y)
+	xdata=np.append(xdata,float(x))
+	ydata=np.append(ydata,float(y))
 print xdata
 print ydata
-
-#change datatype of xdata array:
-xdata = map(np.float64, xdata)
-
-interpolation=[]
-expected=[]
-
+data.close()
 
 #linear interpolation
-class linearinterpolation:
-	def __init__(self,x_in,y_in):
-		for i in range(len(x_in)):
-			if i==(len(x_in)-1):
-				slope=(y_in[i+1]-y_in[i])/(x_in[i+1]-x_in[i])
-				interpolation = 	
-				interpolation.append(linearfit)
-		self.interpolation
-	def linearmodel(self):
-		return self.interpolation
-"""
-	def linearexpected(self,x):
-		x = map(np.float64,x)	
-		for j in range(len(x)):	
-				A=(x_in[i+1]-x)/(x_in[i+1]-x_in[i])
-				B=1.-A	
-				linearfit=A*y_in[i]+B*y_in[i+1]	
-			expected.append(self.interpolation)
-		return expected
-"""	
-			
-linear_interpolation = linearinterpolation(xdata,ydata)
+def interpolate(x):
+	index1=0
+	index2=0
+	if(x<xdata[0]):
+		index1=0
+		index2=1
+	if(x>xdata[len(xdata)-1]):
+		index1=len(xdata)-2
+		index2=len(xdata)-1
+	for i in range(1,len(xdata)-1):
+		if(x>xdata[i-1])and(x<xdata[i]):
+			index1=i-1
+			index2=i
+	
+	slope=(ydata[index2]-ydata[index1])/(xdata[index2]-xdata[index1])
+	b=ydata[index1]-slope*xdata[index1]
+	
+	return slope*x+b
+
+linear_interpolation = interpolate(0.4)
+xref=np.arange(-2.1,3.8,0.1)
+print xref
+
+
 
 #cubic interpolation
-cubic_interpolation = interp1d(xdata,ydata, kind="cubic")
+
 
 
 #plotting
+
 import matplotlib.pyplot as plt
-plt.plot(xdata, ydata, 'o', xdata, linear_interpolation(xdata), '-',xdata, cubic_interpolation(xdata), '--')
-plt.legend(['data', 'linear','cubic'], loc='best')
+plt.plot(xdata, ydata, 'o', xref, linear_interpolation(xref), '-')
+plt.legend(['data', 'linear'], loc='best')
 plt.show()
+
 
 #task 1.a):
 print "Expected value of linear interpolation:"
@@ -69,12 +67,12 @@ print "x=0.4:",linear_interpolation(0.4).round(4)
 print "x=-0.128:",linear_interpolation(-0.128).round(4)
 print "x=-2.0:",linear_interpolation(-2.0).round(4)
 print "x=3.2:",linear_interpolation(3.2).round(4)
-
+"""
 #task 1.b):
 print "Expected value of cubic interpolation:"
 print "x=0.4:",cubic_interpolation(0.4).round(4)
 print "x=-0.128:",cubic_interpolation(-0.128).round(4)
 print "x=-2.0:",cubic_interpolation(-2.0).round(4)
 print "x=3.2:",cubic_interpolation(3.2).round(4)
+"""
 
-data.close()

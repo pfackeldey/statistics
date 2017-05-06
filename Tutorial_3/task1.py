08f660b764491344624e50ce4fc69bc9710feb83
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-05-06 19:36:23 +0200

    update on matrix calculation

--- Tutorial_3/task1.py
+++ Tutorial_3/task1.py
@@ -55,25 +55,30 @@ yref=interpolate(xref)
 a=np.zeros(len(xdata)-1)
 b=np.zeros(len(xdata))
 c=np.zeros(len(xdata)-1)
-F=np.array([])
+F=np.zeros(len(xdata))
 
-matrix=np.zeros(len(xdata),len(xdata))
+matrix=np.zeros((len(xdata),len(xdata)))
 
 #cubic interpolation
-for i in range(1,len(xdata)):
+for i in range(1,len(xdata)-1):
 	a[i]=1./6.*(xdata[i]-xdata[i-1])
-	matrix[i][i+1]=a[i]
+	matrix[i-1][i]=a[i]
 for i in range(1,len(xdata)-1):		
+	b[0]=1./3.*(xdata[1]-xdata[0])#boundary conditions	
 	b[i]=1./3.*(xdata[i+1]-xdata[i-1])
-	matrix[i][i]=b[i]
-for i in range(len(xdata)-1):
+	matrix[i-1][i+1]=b[i]
+for i in range(len(xdata)-2):
 	c[i]=1./6.*(xdata[i+1]-xdata[i])
 	matrix[i+1][i]=c[i]
-for i in range(1,len(xdata)-1):
+for i in range(1,len(xdata)-2):
 	F[i]=(ydata[i+1]-ydata[i])/(xdata[i+1]-xdata[i])-(ydata[i]-ydata[i-1])/(xdata[i]-xdata[i-1])
-	F=np.append(F, float(F[i]))
-	F=np.transpose(F)
+	
+matrix[0][0]=b[0]
+F[0]=0.
+F[-1]=0.
+F.transpose()
 print matrix
+print F
 	
 def cubic_interpolation(x):
 	return 0.

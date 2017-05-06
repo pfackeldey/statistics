1642192ec7ff2be2c3eeb36c3e528dce8fb531ff
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-05-06 14:07:39 +0200

    added cubic interpolation

--- Tutorial_3/task1.py
+++ Tutorial_3/task1.py
@@ -26,14 +26,28 @@ xdata = map(np.float64, xdata)
 #linear interpolation
 linear_interpolation = interp1d(xdata,ydata)
 
+#cubic interpolation
+cubic_interpolation = interp1d(xdata,ydata, kind="cubic")
+
 
 #plotting
 import matplotlib.pyplot as plt
-plt.plot(xdata, ydata, 'o', xdata, linear_interpolation(xdata), '-')
-plt.legend(['data', 'linear'], loc='best')
+plt.plot(xdata, ydata, 'o', xdata, linear_interpolation(xdata), '-',xdata, cubic_interpolation(xdata), '--')
+plt.legend(['data', 'linear','cubic'], loc='best')
 plt.show()
 
 #task 1.a):
-print 
+print "Expected value of linear interpolation:"
+print "x=0.4:",linear_interpolation(0.4).round(4)
+print "x=-0.128:",linear_interpolation(-0.128).round(4)
+print "x=-2.0:",linear_interpolation(-2.0).round(4)
+print "x=3.2:",linear_interpolation(3.2).round(4)
+
+#task 1.b):
+print "Expected value of cubic interpolation:"
+print "x=0.4:",cubic_interpolation(0.4).round(4)
+print "x=-0.128:",cubic_interpolation(-0.128).round(4)
+print "x=-2.0:",cubic_interpolation(-2.0).round(4)
+print "x=3.2:",cubic_interpolation(3.2).round(4)
 
 data.close()

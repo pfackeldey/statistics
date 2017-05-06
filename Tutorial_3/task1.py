1804820f75af57712062c15a9aedf12d87760178
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-05-06 13:54:44 +0200

    added plotting and interpolation

--- Tutorial_3/task1.py
+++ Tutorial_3/task1.py
@@ -1,7 +1,10 @@
 import numpy as np
+from scipy.interpolate import interp1d
 
+#open data
 data = open("data.txt", "r")
 
+#read data and save it in np arrays
 get_data = [line.strip().split() for line in data]
 
 print get_data
@@ -16,5 +19,21 @@ for i in range(len(get_data)):
 	ydata.append(y)
 print xdata
 print ydata
+
+#change datatype of xdata array:
+xdata = map(np.float64, xdata)
+
+#linear interpolation
+linear_interpolation = interp1d(xdata,ydata)
+
+
+#plotting
+import matplotlib.pyplot as plt
+plt.plot(xdata, ydata, 'o', xdata, linear_interpolation(xdata), '-')
+plt.legend(['data', 'linear'], loc='best')
+plt.show()
+
+#task 1.a):
+print 
 
 data.close()

b8ace9d03a64c8e4248648771c4095573ab3c132
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-05-29 17:53:20 +0200

    plotting

--- Tutorial_5/task2.py
+++ Tutorial_5/task2.py
@@ -4,6 +4,8 @@ import random
 from matplotlib import pyplot as plt
 
 
+
+
 #distance between 2 points:
 def distance(A,B):
 	return np.sqrt((B[0] - A[0])**2 + (B[1] - A[1])**2)
@@ -17,8 +19,7 @@ def side_length(r):
 #point on surface:
 P = (0.,1.)
 count = 0.
-n=10000
-
+n=1000
 #TASK A:
 for i in range(n):
 	r=np.sqrt(random.random())
@@ -30,32 +31,39 @@ for i in range(n):
 	else:
 		pass
 
-
 print "Result of task a: ",count/float(n)
 
 #TASK B:
-# P =1-1/3 * (CIRCLE AREA - TRIANGLE ARE)
-P=1. - 1./3. * (np.pi*1.**2. - np.sqrt(3.)/4. * side_length(1.)**2.)
+# P =1/3 * (CIRCLE AREA - TRIANGLE ARE)/CIRCLE AREA
+P=1./3. * (np.pi*1.**2. - np.sqrt(3.)/4. * side_length(1.)**2.)/(np.pi*1.**2.)
 print "Analytical calculated probability is: ",P
 
 
 #TASK C:
 #FIRST METHOD
 count = 0.
-n=1000
 r=1.
+plt.figure(1)
 
 for i in range(n):
 	alpha = random.random()*(2*np.pi)
-	x,y = r*np.cos(alpha), r*np.sin(alpha)
-	new_P = (x,y)
+	x1,y1 = r*np.cos(alpha), r*np.sin(alpha)
+	new_P = (x1,y1)
 	alpha2 = random.random()*(2*np.pi)
 	x2,y2 = r*np.cos(alpha2), r*np.sin(alpha2)
 	new_P2 = (x2,y2)
 	if distance(new_P2,new_P)>side_length(1.):
+		plt.plot([x1,x2],[y1,y2], color = 'red')		
 		count += 1.
 	else:
-		pass
+		plt.plot([x1,x2],[y1,y2], color = 'green')
+
+#circle plot:
+plt.Circle((0, 0), 1., fill=False, color='blue')
+#triangle plot:
+plt.grid()
+plt.show()
+
 
 print "Result of first method: ",count/float(n)
 
@@ -64,8 +72,9 @@ print "Result of first method: ",count/float(n)
 #middle of radius: 
 P = (0,1)
 count = 0.
-n=10000
 r=1.
+
+plt.figure(2)
 
 alpha = random.random()*(2*np.pi)
 beta = abs(alpha-np.pi/2)
@@ -81,16 +90,24 @@ for i in range(n):
 	P1 = (x1,y1)
 	P2 = (x2,y2)
 	if distance(P1,P2)>side_length(1.):
+		plt.plot([x1,x2],[y1,y2], color = 'red')		
 		count += 1.
-	else: 
-		pass
+	else:
+		plt.plot([x1,x2],[y1,y2], color = 'green')
+
+#circle plot:
+plt.Circle((0, 0), 1., fill=False, color='blue')
+#triangle plot:
+plt.grid()
+plt.show()
 
 print "Result of second method: ",float(count)/float(n)
 	
 
 #THIRD METHOD
 count = 0.
-n=10000
+
+plt.figure(1)
 
 for i in range(n):
 	#Point on big circle:
@@ -103,9 +120,16 @@ for i in range(n):
 	x2,y2 = r_small*np.cos(alpha), r_small*np.sin(alpha)
 	P2 = (x2,y2)
 	if distance(P1,(0.,0.))<distance(P2,(0.,0.)):
-		count += 1
+		plt.plot([x1,x2],[y1,y2], color = 'red')		
+		count += 1.
 	else:
-		pass
+		plt.plot([x1,x2],[y1,y2], color = 'green')
+
+#circle plot:
+plt.Circle((0, 0), 1., fill=False, color='blue')
+#triangle plot:
+plt.grid()
+plt.show()
 
 print "Result of third method: ",float(count)/float(n)	
 

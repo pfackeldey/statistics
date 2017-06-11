dc2f64dbe76c71477039b7320373b36d7fb15a8a
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-06-11 13:55:22 +0200

    update on task 3

--- Tutorial_6/fit.cc
+++ Tutorial_6/fit.cc
@@ -17,8 +17,6 @@
 using namespace std;
 
 
-
-
 int fit()
 {
 	//create TCanvas object on which we will draw the histogram, the constructor has as arguments the name, title, and size of canvas in x and y in number of pixels
@@ -26,33 +24,29 @@ int fit()
 	// Use DrawFrame function to plot the frame independent from the content
 	c1->DrawFrame(0,0,200,10000,"; mass [GeV] ; Events / 2 GeV");
 
-	Double_t first_exp(double x) {
-		return [0]*ROOT::Math::exponential_pdf(x,[1]);
-	};
-	Double_t gaussian(double x) {
-		return ROOT::Math::gaussian_pdf(x,[2],[3]);
-	};
-	Double_t second_exp(double x) {
-		return [4]*ROOT::Math::exponential_pdf(x,[5]);
-	};
 
-	TF1 *f1 = new TF1("f1","first_exp(x)+gaussian(x)+second_exp(x)",0,200);
+	TF1 *f1 = new TF1("f1","[0]*ROOT::Math::exponential_pdf(x,[1])+gaus(2)",0,200);
 	// set parameters to the values of the generated function
 	f1->SetParameter(0,50000); // normalization of first exponential part
 	f1->SetParameter(1,2); // parameter lambda of first exponential falloff
-	f1->SetParameter(2,7.5);// parameter of gaussian sigma
-	f1->SetParameter(3,50);   // parameter of gaussian mean
-	f1->SetParameter(4,500);// normalization of second exponential part
-	f1->SetParameter(5,2); // parameter lambda of second exponential falloff
-	// constrain exponential falloff in fit
+	f1->SetParameter(2,10000); // normalization of gaussian part
+	f1->SetParameter(3,50);// parameter of gaussian mean
+	f1->SetParameter(4,1);   // parameter of gaussian sigma
+	// constrain parameter limits
 	f1->SetParLimits(1,0,10);
-	f1->SetParLimits(5,0,10)
+	f1->SetParLimits(4,1,10);
+	f1->SetParLimits(3,45,55);
+
 	//read histograms by name
 	TFile *file =new TFile("histos_ex3.root","READ");
 	TH1F *hist = (TH1F*) file->Get("hist");
 	hist->Fit("f1");
 	hist->Draw("same");
 	c1->Print("fit_solution.pdf");
+	Double_t chi2_ndof = f1->GetChisquare()/f1->GetNDF();
+	Double_t chi2 = f1->GetChisquare();
+	std::cout << "chi2: "<< chi2 << std::endl;
+	std::cout << "chi2/ndof: "<< chi2_ndof << std::endl;
 	return 0;
 }
 

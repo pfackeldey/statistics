cd0bfb4bcbd1cf84616641f46509c8bf2a5167b9
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-06-11 11:12:07 +0200

    added task1.py [WIP]

---
+++ Tutorial_6/task1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import numpy as np
+
+
+def bisection(x1,x2,cut):
+    def func(x):
+        return (x**4.)/300. + (x**3.)/50. - (x**2.)/3. + x/10. +5./2.
+    i=1
+    while i<100:
+        x = (x1+x2)/2.0
+        print "x1="+str(x1)+" ; x2="+str(x2)+" ; x="+str(x)+" ; f(x)="+str(func(x))
+        if (func(x)==0 or abs(x1-x2)/2.0<=cut):
+            break
+        else:
+            i=i+1
+            if func(x1)*func(x2)>0:
+                x1 = x
+            else:
+                x2 = x
+    return x
+
+
+print bisection(-5.,0.,0.001)

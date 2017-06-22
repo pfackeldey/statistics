f618350cb4c66fdcdbfe8addab1f7528c2acc403
SWuchterl <sebastian.wuchterl@rwth-aachen.de>
2017-06-22 13:19:55 +0200

    added comments in hastings / plotting in task1

--- Tutorial_7/hastings.py
+++ Tutorial_7/hastings.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 
+#target function (2 2dim gaussians multiplied)
 def func(x,y):
     sigma_x1=1.5
     sigma_y1=1.    
@@ -24,31 +25,30 @@ def func(x,y):
     gauss_2D_2 = mlab.bivariate_normal(x, y, sigma_x2, sigma_y2, mu_x2, mu_y2,sigma_xy2)
     return 0.4*gauss_2D_1+25.*gauss_2D_2/(0.4+25.)
  
-    
-N=1000
-x_ini = np.zeros(2)
-p_ini = func(x_ini[0], x_ini[1])
+
+# Metropolis hastings
+N=3000
+x = np.zeros(2)
+p = func(x[0], x[1])
 data=[]
 
-x=x_ini
-p=p_ini
-
 for i in range(N):
-    x_new = x + np.random.normal(size=2)
+    x_new = x + np.random.normal(size=2) # gaussian proposal function (2dim)
     p_new = func(x_new[0], x_new[1])
-    a=min(1.,p_new/p)  
+    a=p_new/p
     if a >= 1.:
         x = x_new
         p = p_new
     else:
         P = np.random.rand()
-        if P < a:
+        u=min(1.,p_new/p)
+        if P < u:
             x = x_new
             p = p_new
     data.append(x)
     
 
-
+# PLOTTING
 data = np.array(data)
 plt.scatter(data[:, 0], data[:, 1], alpha=0.5, s=1)
 dx = 0.01
